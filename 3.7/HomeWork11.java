06d4294c1167aa1aa15cb53fa39be476d1f8c42c
WithuL <1509586319@qq.com>
2020-03-09 08:40:27 +0800

    修正

--- 3.7/HomeWork11.java
+++ 3.7/HomeWork11.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class HomeWork11 {
 	
 	public static void main(String[] args) {
-		for(int i = 1;i < 10; i++) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for(int i = 1;i <= n; i++) {
 			int sum = 0;
 			for(int j=1;j <= i;j++) {
 				System.out.print(j+"*"+i+"="+(j*i)+" ");
