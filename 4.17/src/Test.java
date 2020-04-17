49eb810448d25245d2bd04d03e9273460ed0a1a5
WithuL <1509586319@qq.com>
2020-04-17 21:46:02 +0800

    字符串匹配问题

---
+++ 4.17/src/Test.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class Test {
+
+    class Solution {
+        public boolean isValid(String s) {
+            if(s.isEmpty()){
+                return true;
+            }
+            Stack<Character> stack = new Stack<>();
+            for(int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if(c == '[' || c == '(' || c == '{' ){
+                    stack.push(c);
+                    continue;
+                }
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                Character top = stack.pop();
+                if(c == ']' && top == '[') {
+                    continue;
+                }
+                if(c == ')' && top == '(') {
+                    continue;
+                }
+                if(c == '}' && top == '{') {
+                    continue;
+                }
+                return false;
+            }
+            if(stack.isEmpty()) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
